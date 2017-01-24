b1e6148dff765cb119d93d4b179cba429cbfc8f6
Simon Blackwell <syblackwell@anywhichway.com>
2017-01-23 19:16:21 -0500

    added auth

--- cachegit.js
+++ cachegit.js
@@ -1,17 +1,20 @@
 const fs = require("fs"),
 	fetch = require("fetch"),
+	git = require('git-rev'),
 	location = process.argv[2],
 	str = fs.readFileSync((location ? location : "") + "cachegit.json");
 try {
-	const config = JSON.parse(str);
-	config.files.forEach((file) => {
-		const location = "/"+config.account+"/"+config.repository+"/"+file;
-		fetch.fetchUrl("http://cachegit.com/_?"+location,{method:"delete"},(err,metadata,body) => {
-			if(err) {
-				console.log(err);
-			} else {
-				console.log(body.toString(),location);
-			}
+	git.branch((branch) => {
+		const config = JSON.parse(str);
+		config.files.forEach((file) => {
+			const location = "/" + config.account + "/" + config.repository + "/" + file + "?cachegit=" + branch;
+			fetch.fetchUrl("https://" + config.account + ":none@cachegit.com" + location,{method:"delete"},(err,metadata,body) => {
+				if(err) {
+					console.log(err);
+				} else {
+					console.log(body.toString(),location);
+				}
+			});
 		});
 	});
 } catch(e) {
